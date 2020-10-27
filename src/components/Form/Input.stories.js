5170b90bce46499e5438f14b32b54254f39bb604
jmtes <juliennetesoro@gmail.com>
2020-10-27 11:59:48 -0700

    Add descriptions to each story

--- src/components/Form/Input.stories.js
+++ src/components/Form/Input.stories.js
@@ -11,7 +11,8 @@ export default {
   parameters: {
     docs: {
       description: {
-        component: 'A component that accepts and validates user input.'
+        component:
+          'A component that accepts and validates user input. Note that, because input value changes and validation are managed by a parent `Form` component, the following examples do not work functionally and are meant to only give an idea of what each input field variant used in the app looks like. Please refer to the relevant form component, listed in the description of each story, to see the input validation in action.'
       }
     }
   },
@@ -72,122 +73,134 @@ Default.args = {
   id: 'test',
   label: 'Input',
   placeholder: 'Placeholder',
-  validateInput: (input) => {
-    if (!input) return 'Input cannot be empty';
-    else if (input.length < 2) return 'Input is too short';
-    return null;
-  }
+  value: '',
+  onChange: () => {},
+  onBlur: () => {}
 };
 
 export const Name = Template.bind({});
 Name.args = {
+  ...Default.args,
   id: 'name',
   label: 'Name',
   type: 'text',
   placeholder: 'Rick',
-  options: { autoComplete: 'off' },
-  validateInput: (input) => {
-    action('Input Blur')();
-
-    if (input.length < 2) return 'Must be at least 2 characters';
-    else if (input.length > 32) return 'Must not exceed 32 characters';
-    else return null;
+  options: { autoComplete: 'off' }
+};
+Name.parameters = {
+  docs: {
+    storyDescription:
+      'Input that accepts a user name, used in `SignupForm`. Displays validation error if name is shorter than 2 characters or longer than 32.'
   }
 };
 
 export const Email = Template.bind({});
 Email.args = {
+  ...Default.args,
   id: 'email',
   label: 'Email',
   type: 'email',
-  placeholder: 'rick@gmail.com',
-  validateInput: (input) => {
-    action('Input Blur')();
-
-    if (!isEmail(input)) return 'Invalid email address';
-    return null;
+  placeholder: 'rick@gmail.com'
+};
+Email.parameters = {
+  docs: {
+    storyDescription:
+      'Input that accepts a user email address, used in `LoginForm` and `SignupForm`. Displays validation error if field is empty or if an invalid email was entered.'
   }
 };
 
 export const Password = Template.bind({});
 Password.args = {
+  ...Default.args,
   id: 'password',
   label: 'Password',
   type: 'password',
   placeholder: 'Must contain at least 8 characters',
-  options: { minLength: 8 },
-  validateInput: (input) => {
-    action('Input Blur')();
-
-    if (input.length < 8) return 'Must contain at least 8 characters';
-    return null;
+  options: { minLength: 8 }
+};
+Password.parameters = {
+  docs: {
+    storyDescription:
+      'Input that accepts a user password, used in `LoginForm` and `SignupForm`. Displays validation error if password is shorter than 8 characters.'
   }
 };
 
 export const ConfirmPassword = Template.bind({});
 ConfirmPassword.args = {
+  ...Default.args,
   id: 'confirm-password',
   label: 'Confirm Password',
   type: 'password',
-  placeholder: 'Must match previous field',
-  validateInput: (input) => {
-    action('Input Blur')();
-
-    return null;
+  placeholder: 'Must match previous field'
+};
+ConfirmPassword.parameters = {
+  docs: {
+    storyDescription:
+      'Input that accepts a user password, used in `SignupForm`. Displays validation error if password does not match that which had been entered in the `Password` input.'
   }
 };
 
 export const Location = Template.bind({});
 Location.args = {
+  ...Default.args,
   id: 'query',
   label: 'Location',
   type: 'text',
   placeholder: 'Los Angeles, California',
-  icon: 'location',
-  validateInput: (input) => {
-    action('Input Blur')();
-
-    return null;
+  icon: 'location'
+};
+Location.parameters = {
+  docs: {
+    storyDescription:
+      'Input that accepts a location name (city, state, country), used in `SearchForm`. Displays validation error if field is left empty.'
   }
 };
 
 export const Checkin = Template.bind({});
 Checkin.args = {
+  ...Default.args,
   id: 'checkin',
   label: 'Checkin',
   type: 'date',
   icon: 'calendar',
-  width: '50%',
-  validateInput: () => {
-    action('Input Blur')();
+  width: '50%'
+};
+Checkin.parameters = {
+  docs: {
+    storyDescription:
+      "Input that accepts the date on which a user plans on checking in to a listing, used in `SearchForm` and `ReservationForm`. Displays validation error in the following scenarios: field is left empty, input is not in YYYY-MM-DD format, date is invalid (e.g. `2020-10-32`), date is before today's date."
   }
 };
 
 export const Checkout = Template.bind({});
 Checkout.args = {
+  ...Default.args,
   id: 'checkout',
   label: 'Checkout',
   type: 'date',
   icon: 'calendar',
-  width: '50%',
-  validateInput: () => {
-    action('Input Blur')();
+  width: '50%'
+};
+Checkout.parameters = {
+  docs: {
+    storyDescription:
+      'Input that accepts the date on which a user plans on checking out of a listing, used in `SearchForm` and `ReservationForm`. Displays validation error in the following scenarios: field is left empty, input is not in YYYY-MM-DD format, date is invalid (e.g. `2020-10-32`), date is before date entered in `Checkin` input.'
   }
 };
 
 export const Guests = Template.bind({});
 Guests.args = {
+  ...Default.args,
   id: 'guests',
   label: 'Guests',
   type: 'number',
   placeholder: '1',
   icon: 'person',
-  options: { min: 1 },
-  validateInput: (input) => {
-    action('Input Blur')();
-
-    if (!Number.isInteger(parseFloat(input, 10)))
-      return 'Number must be an integer';
-    return null;
+  options: { min: 1 }
+};
+Guests.parameters = {
+  docs: {
+    storyDescription:
+      'Input that accepts the projected number of guests to stay at the listing, used in `SearchForm` and `ReservationForm`. Displays validation error in the following scenarios: field is left empty, input is less than 1, input is a non-integer.'
   }
 };
